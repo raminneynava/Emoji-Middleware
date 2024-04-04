c4236ba13bbc3b63b174a664b86f5e2efae2a3ec
raminneynava <raminneynava@gmail.com>
2024-04-04 19:59:21 +0330

    Add project files.

---
+++ Emoji-Middleware/Components/EmojiMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Emoji_Middleware.Components
+{
+    public static class EmojiMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseEmojiMiddleware(
+        this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<EmojiMiddleware>();
+        }
+    }
+
+}
---
+++ Emoji-Middleware/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Emoji_Middleware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
